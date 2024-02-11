f2d587f8cd51895d483ba61c4200b2824d9dfd47
Vishesh Koul <visheshkoul@gmail.com>
2024-02-11 20:01:22 +0530

    Updated countdown and pther fixes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,34 @@
 ﻿$(window).on('load', function() { 
-});
 
+});
+var countDownDate = new Date('2024-02-22T18:30:11.000Z');
+
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "Happily Married";
+  }
+}, 1000);
 
 (function(){
   var myDiv = document.getElementById("se-pre-con"),
